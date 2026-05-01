b995b909ecef3b581b54d4d16a3e90f2344b4bd7
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-05-01 18:00:16 +0430

    new migrations added

---
+++ database/migrations/2026_05_01_132616_create_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string("last_name");
+            $table->foreignId("user_id")->constrained("users", "id");
+            $table->string("image_url")->nullable();
+            $table->string("phone_numbe");
+            $table->string("tazkira_no")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
